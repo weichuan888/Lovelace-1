bcb33be9a4cd3ac6d17668b484d5f63b4da680d1
bertrandBecode <bertrand@becode.org>
2017-12-06 11:50:53 +0100

    react

---
+++ Parcours/ReactJs/1-ChatBox/EndChatBox/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyCvp3pE7XX04-SRPgUOhmwxBWBR0_fQnlo",
+	authDomain: "chatlovelace-14166.firebaseapp.com",
+	databaseURL: "https://chatlovelace-14166.firebaseio.com"
+});
+
+export default base;
